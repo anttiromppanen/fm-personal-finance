9bbbf240f39632da7e7ac7b9718672bb11efc776
Antti Romppanen <romppanen.antti@gmail.com>
2024-10-06 22:48:14 +0300

    update sign up test

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -7,8 +7,28 @@ test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/register");
 });
 
+test.afterAll(async () => {
+  await fetch("http://localhost:3000/api/repopulate-users", {
+    method: "POST",
+  });
+});
+
 test.describe("Signup", () => {
-  test("should allow a user to sign up", async ({ page }) => {
+  test("should allow a new user to sign up", async ({ page }) => {
     await expect(page.getByRole("heading", { name: "Sign Up" })).toBeVisible();
+
+    const emailFiend = page.getByPlaceholder("Enter email address");
+    const passwordField = page.getByPlaceholder("Password");
+    const submitButton = page.getByRole("button", { name: "Sign up" });
+
+    await emailFiend.fill("testuser666@test.com");
+    await passwordField.fill("Password123");
+    await submitButton.click();
+
+    await page.waitForLoadState("domcontentloaded");
+    await expect(page).toHaveURL("http://localhost:3000/");
+    await expect(page.getByRole("heading", { name: "Overview" })).toBeVisible();
+
+    await page.click("text=Logout");
   });
 });
