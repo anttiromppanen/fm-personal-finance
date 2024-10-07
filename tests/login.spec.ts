5f31aac3acc993a72b9c8b09b41ca98dedcafba9
Antti Romppanen <romppanen.antti@gmail.com>
2024-10-07 22:50:04 +0300

    refactor login tests & added email validation tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,15 @@
 import { usersWithValidCredentials } from "@/utils/data/mockUserData";
-import { expect, test } from "@playwright/test";
+import { expect, Page, test } from "@playwright/test";
+
+async function handleForm(page: Page, email: string, password: string) {
+  const emailField = page.getByPlaceholder("Enter email address");
+  const passwordField = page.getByPlaceholder("Password");
+  const submitButton = page.getByRole("button", { name: "Log In" });
+
+  await emailField.fill(email);
+  await passwordField.fill(password);
+  await submitButton.click();
+}
 
 test.describe("Login", () => {
   test("should initially be redirected to /login", async ({ page }) => {
@@ -11,13 +21,7 @@ test.describe("Login", () => {
     const validUser = usersWithValidCredentials[0];
     await page.goto("http://localhost:3000/login");
 
-    const emailField = page.getByPlaceholder("Enter email address");
-    const passwordField = page.getByPlaceholder("Password");
-    const submitButton = page.getByRole("button", { name: "Log In" });
-
-    await emailField.fill(validUser.email);
-    await passwordField.fill(validUser.password);
-    await submitButton.click();
+    await handleForm(page, validUser.email, validUser.password);
 
     await expect(page).toHaveURL("http://localhost:3000/");
     await expect(page.getByRole("heading", { name: "Overview" })).toBeVisible();
@@ -32,15 +36,62 @@ test.describe("Login", () => {
     };
     await page.goto("http://localhost:3000/login");
 
-    const emailField = page.getByPlaceholder("Enter email address");
-    const passwordField = page.getByPlaceholder("Password");
-    const submitButton = page.getByRole("button", { name: "Log In" });
-
-    await emailField.fill(notRegisteredUser.email);
-    await passwordField.fill(notRegisteredUser.password);
-    await submitButton.click();
+    await handleForm(page, notRegisteredUser.email, notRegisteredUser.password);
 
     await expect(page).toHaveURL("http://localhost:3000/login");
     await expect(page.getByText("Invalid login credentials")).toBeVisible();
+  });
+
+  // Fields use the default validation popups, so we can't test the error message
+  test.describe("Email validation", () => {
+    test("fails when missing @ symbol", async ({ page }) => {
+      const userWithInvalidEmail = {
+        email: "plainaddress.com",
+        password: "CorrectPassword123",
+      };
+      await page.goto("http://localhost:3000/login");
+
+      await handleForm(
+        page,
+        userWithInvalidEmail.email,
+        userWithInvalidEmail.password,
+      );
+
+      await expect(page).toHaveURL("http://localhost:3000/login");
+    });
+
+    test("fails when missing username before the @ symbol", async ({
+      page,
+    }) => {
+      const userWithInvalidEmail = {
+        email: "@domain.com",
+        password: "idealPassword666",
+      };
+      await page.goto("http://localhost:3000/login");
+
+      await handleForm(
+        page,
+        userWithInvalidEmail.email,
+        userWithInvalidEmail.password,
+      );
+
+      await expect(page).toHaveURL("http://localhost:3000/login");
+    });
+
+    test.only("fails when missing email address", async ({ page }) => {
+      const userWithInvalidEmail = {
+        email: "",
+        password: "idealPassword666",
+      };
+      await page.goto("http://localhost:3000/login");
+
+      await handleForm(
+        page,
+        userWithInvalidEmail.email,
+        userWithInvalidEmail.password,
+      );
+
+      await expect(page).toHaveURL("http://localhost:3000/login");
+    });
   });
 });
